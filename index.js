b0c4167dccfe7bad62f37b899aac6bfcf1a298c1
C. T. Lin <chentsulin@gmail.com>
2015-09-14 19:23:38 +0800

    fix global-shortcut break other apps

--- index.js
+++ index.js
@@ -6,11 +6,21 @@ const isOSX = process.platform === 'darwin';
 
 module.exports = function () {
 	app.on('ready', function () {
-		globalShortcut.register(isOSX ? 'Cmd+Alt+I' : 'Ctrl+Shift+I', devTools);
-		globalShortcut.register('F12', devTools);
+		app.on('browser-window-focus', function() {
+			globalShortcut.register(isOSX ? 'Cmd+Alt+I' : 'Ctrl+Shift+I', devTools);
+			globalShortcut.register('F12', devTools);
 
-		globalShortcut.register('CmdOrCtrl+R', refresh);
-		globalShortcut.register('F5', refresh);
+			globalShortcut.register('CmdOrCtrl+R', refresh);
+			globalShortcut.register('F5', refresh);
+		});
+		
+		app.on('browser-window-blur', function() {
+			globalShortcut.unregister(isOSX ? 'Cmd+Alt+I' : 'Ctrl+Shift+I');
+			globalShortcut.unregister('F12');
+			
+			globalShortcut.unregister('CmdOrCtrl+R');
+			globalShortcut.unregister('F5');
+		});
 
 		function devTools() {
 			var win = BrowserWindow.getFocusedWindow();

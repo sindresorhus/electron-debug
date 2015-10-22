85a2c96aadf071715322dd25155feabe82b668e3
Andrea Parodi <andrea@parro.it>
2015-10-22 18:58:30 +0200

    implemented inspect element context menu as per #9

--- index.js
+++ index.js
@@ -20,22 +20,49 @@ function refresh() {
 	}
 }
 
+function activateDebugContextMenu(e) {
+	const webContents = e.sender;
+	webContents.executeJavaScript(`require('${__dirname}/debug-context-menu').install();`);
+}
+
+function deactivateDebugContextMenu(e) {
+	const webContents = e.sender;
+	webContents.executeJavaScript(`require('${__dirname}/debug-context-menu').uninstall();`);
+}
+
+function installDebugContextMenu(win) {
+	win.webContents.on('devtools-opened', activateDebugContextMenu);
+	win.webContents.on('devtools-closed', deactivateDebugContextMenu);
+	if (win.webContents.isDevToolsOpened()) {
+		activateDebugContextMenu({sender: win.webContents});
+	}
+}
+
+function uninstallDebugContextMenu(win) {
+	win.webContents.removeListener('devtools-opened', activateDebugContextMenu);
+	win.webContents.removeListener('devtools-closed', deactivateDebugContextMenu);
+}
+
 module.exports = function () {
 	app.on('ready', function () {
-		app.on('browser-window-focus', function () {
+		app.on('browser-window-focus', function (e, win) {
 			globalShortcut.register(isOSX ? 'Cmd+Alt+I' : 'Ctrl+Shift+I', devTools);
 			globalShortcut.register('F12', devTools);
 
 			globalShortcut.register('CmdOrCtrl+R', refresh);
 			globalShortcut.register('F5', refresh);
+
+			installDebugContextMenu(win);
 		});
 
-		app.on('browser-window-blur', function () {
+		app.on('browser-window-blur', function (e, win) {
 			globalShortcut.unregister(isOSX ? 'Cmd+Alt+I' : 'Ctrl+Shift+I');
 			globalShortcut.unregister('F12');
 
 			globalShortcut.unregister('CmdOrCtrl+R');
 			globalShortcut.unregister('F5');
+
+			uninstallDebugContextMenu(win);
 		});
 	});
 };

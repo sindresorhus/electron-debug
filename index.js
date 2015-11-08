d34dfe6544b932ff60e157b911d8d93f6b230f92
Andrea Parodi <andrea@parro.it>
2015-11-08 10:29:04 +0100

    created related section

--- index.js
+++ index.js
@@ -5,69 +5,35 @@ const localShortcut = require('electron-localshortcut');
 const isOSX = process.platform === 'darwin';
 
 function devTools(win) {
-	win = win || BrowserWindow.getFocusedWindow();
+  win = win || BrowserWindow.getFocusedWindow();
 
-	if (win) {
-		win.toggleDevTools();
-	}
+  if (win) {
+    win.toggleDevTools();
+  }
 }
 
 function refresh() {
-	const win = BrowserWindow.getFocusedWindow();
+  const win = BrowserWindow.getFocusedWindow();
 
-	if (win) {
-		win.reloadIgnoringCache();
-	}
-}
-
-function activateDebugContextMenu(e) {
-	const webContents = e.sender;
-	webContents.executeJavaScript(`window.__electron_debug.require('${__dirname}/context-menu').install();`);
-}
-
-function deactivateDebugContextMenu(e) {
-	const webContents = e.sender;
-	webContents.executeJavaScript(`window.__electron_debug.require('${__dirname}/context-menu').uninstall();`);
-}
-
-function installDebugContextMenu(win) {
-	win.webContents.on('devtools-opened', activateDebugContextMenu);
-	win.webContents.on('devtools-closed', deactivateDebugContextMenu);
-
-	if (win.webContents.isDevToolsOpened()) {
-		activateDebugContextMenu({sender: win.webContents});
-	}
-}
-
-function uninstallDebugContextMenu(win) {
-	win.webContents.removeListener('devtools-opened', activateDebugContextMenu);
-	win.webContents.removeListener('devtools-closed', deactivateDebugContextMenu);
+  if (win) {
+    win.reloadIgnoringCache();
+  }
 }
 
 module.exports = opts => {
-	opts = opts || {};
+  opts = opts || {};
 
-	app.on('browser-window-created', (e, win) => {
-		if (opts.showDevTools) {
-			devTools(win);
-		}
+  app.on('browser-window-created', (e, win) => {
+    if (opts.showDevTools) {
+      devTools(win);
+    }
+  });
 
-		win.webContents.executeJavaScript('window.__electron_debug = {require: window.require};');
-	});
+  app.on('ready', () => {
+    localShortcut.register(isOSX ? 'Cmd+Alt+I' : 'Ctrl+Shift+I', devTools);
+    localShortcut.register('F12', devTools);
 
-	app.on('ready', () => {
-		localShortcut.register(isOSX ? 'Cmd+Alt+I' : 'Ctrl+Shift+I', devTools);
-		localShortcut.register('F12', devTools);
-
-		localShortcut.register('CmdOrCtrl+R', refresh);
-		localShortcut.register('F5', refresh);
-	});
-
-	app.on('browser-window-focus', (e, win) => {
-		installDebugContextMenu(win);
-	});
-
-	app.on('browser-window-blur', (e, win) => {
-		uninstallDebugContextMenu(win);
-	});
+    localShortcut.register('CmdOrCtrl+R', refresh);
+    localShortcut.register('F5', refresh);
+  });
 };
--- readme.md
+++ readme.md
@@ -27,6 +27,7 @@ Force reload the window.
 
 Right-click on an HTML element when DevTools is open to inspect it.
 
+Note: this features has been moved to [its own package](https://github.com/parro-it/debug-menu). Please refer to it for related issues.
 
 ## Install
 
@@ -41,16 +42,16 @@ $ npm install --save electron-debug
 var app = require('app');
 
 require('electron-debug')({
-	showDevTools: true
+  showDevTools: true
 });
 
 let win;
 
 app.on('ready', function () {
-	win = new BrowserWindow({
-		width: 800,
-		height: 600
-	});
+  win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
 });
 ```
 
@@ -60,6 +61,13 @@ app.on('ready', function () {
 
 If true, DevTools will be open when app has been started.
 
+## Related
+
+- [debug-menu](https://github.com/parro-it/debug-menu) - Chromium-like debug context-menu for [Electron](http://electron.atom.io)
+
+
 ## License
 
 MIT © [Sindre Sorhus](http://sindresorhus.com)
+
+

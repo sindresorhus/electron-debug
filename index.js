a492a8d9067a36a1658acef977386d493f3dcc0d
Andrea Parodi <andrea@parro.it>
2015-11-17 01:34:19 +0700

    Close #22 PR: bump electron-prebuilt dep.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
-const app = require('app');
-const BrowserWindow = require('browser-window');
+const electron = require('electron');
 const localShortcut = require('electron-localshortcut');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 const isOSX = process.platform === 'darwin';
 
 function devTools(win) {
--- test.js
+++ test.js
@@ -1,10 +1,17 @@
 'use strict';
-const app = require('app');
-const BrowserWindow = require('browser-window');
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 require('.')({showDevTools: true});
 
+function load(url) {
+	const win = new BrowserWindow({show: true});
+	win.loadURL(url);
+	win.setMenu(null);
+}
+
 app.on('ready', () => {
-	(new BrowserWindow({show: true})).loadUrl('https://github.com');
-	(new BrowserWindow({show: true})).loadUrl('https://google.com');
+	load('https://github.com');
+	load('https://google.com');
 });

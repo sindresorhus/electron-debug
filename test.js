1f1754a7326714b322362ea1f1a93dfa3898ee35
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-08 23:25:26 +0700

    meta tweaks

--- test.js
+++ test.js
@@ -1,17 +1,15 @@
 'use strict';
 const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
 
 require('.')({showDevTools: true});
 
 function load(url) {
-	const win = new BrowserWindow({show: true});
+	const win = new electron.BrowserWindow({show: true});
 	win.loadURL(url);
 	win.setMenu(null);
 }
 
-app.on('ready', () => {
+electron.app.on('ready', () => {
 	load('https://github.com');
 	load('https://google.com');
 });
